6548a2a5a2b1d012a84860e72d4519a816dbaccf
Ali <ahmedalielm02@gmail.com>
2025-07-02 18:27:42 +0100

    First

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,16 +35,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-            Column(
-                modifier = Modifier.background(Color.Green)
-                    .width(300.dp)
-                    .height(300.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround) {
+//            Column(
+//                modifier = Modifier.background(Color.Green)
+//                    .width(300.dp)
+//                    .height(300.dp),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.SpaceAround) {
+//
+//                Text("Hello there")
+//                Text("Hello there")
+//                Text("Hello there")
+//            }
 
-                Text("Hello there")
-                Text("Hello there")
-                Text("Hello there")
+
+            //---------------------------------------------
+
+
+            Column (
+                modifier = Modifier.background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .padding(top = 50.dp, start = 20.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(10.dp)
+            ) {
+                Text("Hello", modifier = Modifier.clickable {
+                })
+
+                Spacer(modifier = Modifier.height(50.dp))
+
+                Text("World")
             }
         }
     }
