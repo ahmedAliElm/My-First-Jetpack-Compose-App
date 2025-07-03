4eb28cbe74a49aa099b1b612e7b8c27f612f8eb5
Ali <ahmedalielm02@gmail.com>
2025-07-03 17:48:34 +0100

    First

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -67,6 +67,7 @@ import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -228,42 +229,42 @@ class MainActivity : ComponentActivity() {
             //---------------------------------------------
 
 
-            val constraints = ConstraintSet {
-
-                val greenBox = createRefFor("greenbox")
-                val redBox = createRefFor("redbox")
-                val guideline = createGuidelineFromTop(0.5f)
-
-                constrain(greenBox) {
-                    top.linkTo(guideline)
-                    start.linkTo(parent.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-
-                constrain(redBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(greenBox.end)
-                    end.linkTo(parent.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-
-                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
-            }
-
-            ConstraintLayout (constraints, modifier = Modifier.fillMaxSize()) {
-
-                Box (modifier = Modifier
-                    .background(Color.Green)
-                    .layoutId("greenbox")
-                )
-
-                Box (modifier = Modifier
-                    .background(Color.Red)
-                    .layoutId("redbox")
-                )
-            }
+//            val constraints = ConstraintSet {
+//
+//                val greenBox = createRefFor("greenbox")
+//                val redBox = createRefFor("redbox")
+//                val guideline = createGuidelineFromTop(0.5f)
+//
+//                constrain(greenBox) {
+//                    top.linkTo(guideline)
+//                    start.linkTo(parent.start)
+//                    width = Dimension.value(100.dp)
+//                    height = Dimension.value(100.dp)
+//                }
+//
+//                constrain(redBox) {
+//                    top.linkTo(parent.top)
+//                    start.linkTo(greenBox.end)
+//                    end.linkTo(parent.end)
+//                    width = Dimension.value(100.dp)
+//                    height = Dimension.value(100.dp)
+//                }
+//
+//                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+//            }
+//
+//            ConstraintLayout (constraints, modifier = Modifier.fillMaxSize()) {
+//
+//                Box (modifier = Modifier
+//                    .background(Color.Green)
+//                    .layoutId("greenbox")
+//                )
+//
+//                Box (modifier = Modifier
+//                    .background(Color.Red)
+//                    .layoutId("redbox")
+//                )
+//            }
         }
     }
 }
